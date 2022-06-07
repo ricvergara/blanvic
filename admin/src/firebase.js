1cc477a711f48bcd291cc913a1a72f43002f1b38
ricvergara <ricardovergar@gmail.com>
2022-06-07 13:13:28 -0500

    admin, api and client directories

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAryRy1N_enrW7Uk1uwRaJUhBc2RJhgegg",
+  authDomain: "shop-a0e6f.firebaseapp.com",
+  projectId: "shop-a0e6f",
+  storageBucket: "shop-a0e6f.appspot.com",
+  messagingSenderId: "904376181255",
+  appId: "1:904376181255:web:19d9909d9b8e4fe4ab40a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
